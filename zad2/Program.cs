cb2713d8b46bad582aa6799ef1e6811154c6a8b9
HoneyRinn <fyodorov.ilja2017@yandex.ru>
2020-12-07 22:06:49 +0500

    Добавьте файлы проекта.

---
+++ zad2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triug
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            zad2 trg1 = new zad2(7, 7, 7);
+            zad2 trg2 = new zad2(7, 6, 3);
+            zad2 trg3 = new zad2(5, 5, 3);
+            zad2 trg4 = new zad2(4, 5, 3);
+            zad2 trg5 = new zad2(8, 8, 8);
+            zad2 trg6 = new zad2(9, 53, 2);
+
+            Console.WriteLine("Равносторонние треугольники : \n");
+            foreach (string trg in zad2.equalTrgs)
+            {
+                Console.WriteLine(trg + "\n");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Равнобедренные треугольники : \n");
+            foreach (string trg in zad2.esoscleTrgs)
+            {
+                Console.WriteLine(trg + "\n");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Прямоугольные треугольники : \n");
+            foreach (string trg in zad2.rectangleTrgs)
+            {
+                Console.WriteLine(trg + "\n");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Произвольные треугольники : \n");
+            foreach (string trg in zad2.randomTrgs)
+            {
+                Console.WriteLine(trg + "\n");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Треугольники, которые нельзя построить : \n");
+            foreach (string trg in zad2.notTrgs)
+            {
+                Console.WriteLine(trg + "\n");
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ zad2/triug.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace Triug
+{
+    class zad2
+    {
+        private bool triangle;
+        private double square;
+        private double perimeter;
+
+        public static List<string> equalTrgs = new List<string>();
+        public static List<string> esoscleTrgs = new List<string>();
+        public static List<string> rectangleTrgs = new List<string>();
+        public static List<string> randomTrgs = new List<string>();
+        public static List<string> notTrgs = new List<string>();
+
+        public zad2(int l1, int l2, int l3)
+        {
+            double perimeter_ = l1 + l2 + l3;
+            double halfOfP = perimeter / 2;
+            double square_ = Math.Sqrt(halfOfP * (halfOfP - l1) * (halfOfP - l2) * (halfOfP - l3));
+
+            if ((l1 + l2 > l3) && (l2 + l3 > l1) && (l1 + l3 > l2))
+            {
+                triangle = true;
+                square = square_;
+                perimeter = perimeter_;
+
+                if (l1 == l2 && l1 == l3) 
+                {
+                    equalTrgs.Add($"Треугольник a = {l1} b = {l2} c = {l3}");
+                }
+                else if ((l1 == l2 && l1 != l3) || (l2 == l3 && l2 != l1) || (l3 == l1 && l3 != l2)) 
+                {
+                    esoscleTrgs.Add($"Треугольник a = {l1} b = {l2} c = {l3}");
+                }
+                else if ((l1 * l1 + l2 * l2 == l3 * l3) || (l1 * l1 + l3 * l3 == l2 * l2) || (l3 * l3 + l2 * l2 == l1 * l1)) 
+                {
+                    rectangleTrgs.Add($"Треугольник a = {l1} b = {l2} c = {l3}");
+                }
+                else  
+                {
+                    randomTrgs.Add($"Треугольник a = {l1} b = {l2} c = {l3}");
+                }
+
+            }
+            else
+            {        
+                notTrgs.Add($"Треугольник a = {l1} b = {l2} c = {l3}");
+                triangle = false;
+            }
+        }
+    }
+}
